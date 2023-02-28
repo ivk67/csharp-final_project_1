68f3f5b1d8a02078847e354170a5e56a863b36b6
ilya_k <ivk68@list.ru>
2023-02-28 21:10:57 +0300

    выводим введенный массив и полученный результат

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ string Prompt(string message)
     string input = Console.ReadLine();
     return input;
 }
-void ShowArray(string[] array)                                                        // определяем функцию ввода массива
+void ShowArray(string[] array)                                                      // определяем функцию ввода массива
 {
     for (int i = 0; i < array.Length; i++) {
         System.Console.Write($"{array[i]}  ");
@@ -21,7 +21,7 @@ string[] CreateArray()
     string value = String.Empty;
     string[] secondArray = new string[10];
     // System.Console.WriteLine($"Введите 'exit' для завершения ввода:");
-    for (count = 0; value != "exit"; count++)                                        // непосредственный ввод массива с клавиатуры
+    for (count = 0; value != "exit"; count++)                                      // непосредственный ввод массива с клавиатуры
     {
         value = Prompt($"Введите {count+1}-е значение или введите 'exit' для завершения ввода ");
         if (value != "exit") {
@@ -39,13 +39,13 @@ string[] CheckArray(string[] array)
     int count = 0;
     int index = 0;
     for (int i = 0; i < array.Length; i++) 
-    {                                                                                // Перебор массива по элементам
+    {                                                                             // Перебор массива по элементам
         if (array[i].Length < 4) count++;
     }
     string[] finalArray = new string[count];
     for (int j = 0; j < array.Length; j++) 
    
- {                                                                               // Проверяем длину введенной строки
+ {                                                                                // Проверяем длину введенной строки
         if (array[j].Length < 4) {
             finalArray[index] = array[j];
             index++;
@@ -53,3 +53,8 @@ string[] CheckArray(string[] array)
     }
     return finalArray;
 }
+string[] array = CreateArray();
+System.Console.Write("Полученные данные: ");                                      // Вывод полученных данных и результата
+ShowArray(array);
+System.Console.Write("Результат: ");
+ShowArray(CheckArray(array));
